a5040e04db3ec5078ca28af1a11e4d31e1e6aa56
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2023-08-26 00:41:06 +0700

    Feat: card name & number validation

--- src/pages/Form/components/CardName.jsx
+++ src/pages/Form/components/CardName.jsx
@@ -1,8 +1,17 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import formData from '../../../formContext'
 
 function CardName() {
-   const { form } = useContext(formData)
+    const { form } = useContext(formData),
+      [nameVal, setNameVal] = useState(''),
+      [isError, setIsError] = useState(false)
+
+    const cardNameHandler = (e) => {
+      const cardName = e.target.value
+      e.target.maxLength = 36
+      setNameVal(cardName)
+      cardName.trim().length === 0 ? setIsError(true) : setIsError(false)
+    }
 
    return (
       <div className="flex flex-col mx-4 my-2">
@@ -10,10 +19,18 @@ function CardName() {
           <input
             type="text"
             name="cardholderName"
+            value={nameVal}
             placeholder={form.cardName}
             className="mt-1.5 px-2.5 py-2"
+            onChange={cardNameHandler}
           />
-        </div>
-   )
+          {isError && (
+            <p className="Error-MSG text-red-500">
+              Can&apos;t be blank
+            </p>
+            )
+          }
+      </div>
+   ); 
 }
 export default CardName
--- src/pages/Form/components/CardNumber.jsx
+++ src/pages/Form/components/CardNumber.jsx
@@ -1,9 +1,31 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import formData from '../../../formContext'
 
 function CardNumber() {
-   const { form } = useContext(formData)
-   const isError = false
+    const { form } = useContext(formData),
+    [numVal, setNumVal] = useState(''),
+    [isError, setIsError] = useState(false),
+    [isEmpty, setIsEmpty] = useState(false)
+
+    // ChatGPT
+    function formatCardNumber(input) {
+      const cleanedInput = input.replace(/\s/g, ''),
+        groups = cleanedInput.match(/.{1,4}/g);
+      return groups ? groups.join(' ') : '';
+    }
+   
+    const cardNumHandler = (e) => {
+      const cardNumber = e.target.value,
+      regEx = /^\d+$/,
+      regExTest = cardNumber.trim(),
+      formatNum = formatCardNumber(cardNumber)
+
+      e.target.maxLength = 19      
+      setNumVal(formatNum)
+      formatNum.trim().length === 0 ? setIsEmpty(true) : setIsEmpty(false)
+      regEx.test(regExTest) || regExTest.length === 0 ? setIsError(false) : setIsError(true)
+      console.log(cardNumber);
+    }
 
    return (
       <div className="flex flex-col mx-4 my-2">
@@ -11,13 +33,20 @@ function CardNumber() {
           <input
             type="text"
             name="cardNumber"
+            value={numVal}
             placeholder={form.cardNum}
             className="mt-1.5 px-2.5 py-2"
+            onChange={cardNumHandler}
           />
           
-          {!isError && (
+          {isError && (
           <p className="Error-MSG py-1 text-red-500">
             Wrong format, numbers only
+          </p>
+          )}
+          {isEmpty && (
+          <p className="Error-MSG py-1 text-red-500">
+            Can&apos;t be blank 
           </p>
           )}
         </div>
